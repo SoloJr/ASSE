1f4585afae1826ed025dd01a2be249e42a707339
Mircea Solovastru <mircea.solo95@yahoo.com>
2021-01-03 20:12:26 +0200

    Added DAL

---
+++ LibraryAdministration/LibraryAdministration/DataAccessLayer/BookPublisherRepository.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using LibraryAdministration.DomainModel;
+using LibraryAdministration.Interfaces.DataAccess;
+
+namespace LibraryAdministration.DataAccessLayer
+{
+    class BookPublisherRepository : BaseRepository<BookPublisher>, IBookPublisherRepository
+
+    {
+    }
+}
---
+++ LibraryAdministration/LibraryAdministration/DataAccessLayer/DomainRepository.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using LibraryAdministration.DomainModel;
+using LibraryAdministration.Interfaces.DataAccess;
+
+namespace LibraryAdministration.DataAccessLayer
+{
+    class DomainRepository : BaseRepository<Domain>, IDomainRepository
+    {
+    }
+}
