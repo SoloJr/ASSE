3ede281e4c91361ef226ffb82988c1798ec28300
Mircea Solovastru <mircea.solo95@yahoo.com>
2021-01-03 20:07:19 +0200

    Added business and data access interfaces

---
+++ LibraryAdministration/LibraryAdministration/Interfaces/Business/IBookRentalService.cs
@@ -0,0 +1,9 @@
+﻿using LibraryAdministration.DomainModel;
+
+namespace LibraryAdministration.Interfaces.Business
+{
+    public interface IBookRentalService : IService<BookRental>
+    {
+        
+    }
+}
